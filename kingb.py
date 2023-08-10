c915ec0f784ad34ee9359cea1b17295e49f4a37d
RagnarBob <131091007+RagnarBob@users.noreply.github.com>
2023-08-11 00:57:23 +0200

    Add files via upload

---
+++ kingb.py
@@ -0,0 +1,29 @@
+while True:
+    nonb = int(input("rantre yon nonb komanse nan 1 rive nan 10 :"))
+    j = 1
+    while(j<=10):
+        pwodwi = nonb * j
+        print(nonb , "X" , j ,"=" , pwodwi) 
+        j+=1
+    repons = input("eskew vle genere yon lot tab iltiplikasyon anko ? peze (n) si non ou (w) si wi :")
+    if(repons== "n" or repons== "N"):
+     break
+    elif(repons== "W" or repons== "w"):
+       continue
+    else:
+       (repons != "w" or repons !="W") and (repons!="n" or repons!="N")
+       print("saw antre an pa bon,rantre n ou w svp :")
+
+
+
+    
+        
+
+
+
+    
+
+    
+    
+
+
